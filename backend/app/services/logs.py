0aa967304b47993fa9169d47de3ae3ade0ddde58
Archie Spiller <83834735+platinumxy@users.noreply.github.com>
2025-03-01 14:21:15 +0000

    Better logging

--- backend/app/services/logs.py
+++ backend/app/services/logs.py
@@ -1,31 +1,26 @@
 from threading import Lock
+from datetime import datetime
 
 
 class Logger: 
     def __init__(self) -> None:
         self.__lock = Lock()
     
-    def error(self, module: str, e: Exception) -> None: 
+    def __log(self, level: str, module: str, msg: str) -> None:
         with self.__lock:
-            print(f"ERROR: {module} - {e}")
+            print(f"{datetime.now().strftime(r'%y/%m/%d %H:%M:%S')} [{level}]: {module} - {msg}")
+
+    def error(self, module: str, e: Exception) -> None: 
+        self.__log("E", module, e.__str__())
 
     def info(self, module: str, msg: str) -> None:
-        with self.__lock:
-            print(f"INFO: {module} - {msg}")
+        self.__log("I", module, msg)
+    
+    def warn(self, module: str, msg: str) -> None:
+        self.__log("W", module, msg)
     
     def debug(self, module: str, msg: str) -> None:
-        with self.__lock:
-            print(f"DEBUG: {module} - {msg}")
+        self.__log("D", module, msg)
 
-    def warning(self, module: str, msg: str) -> None:
-        with self.__lock:
-            print(f"WARNING: {module} - {msg}")
-        
     def critical(self, module: str, msg: str) -> None:
-        with self.__lock:
-            print(f"CRITICAL: {module} - {msg}")
-    
-    def success(self, module: str, msg: str) -> None:
-        with self.__lock:
-            print(f"SUCCESS: {module} - {msg}")
-    
+        self.__log("C", module, msg)
