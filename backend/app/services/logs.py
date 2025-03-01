55a84f5ed6371307355c5f1f61a3e4e5cecb20fd
Archie Spiller <83834735+platinumxy@users.noreply.github.com>
2025-03-01 14:20:51 +0000

    Better logging

--- backend/app/services/logs.py
+++ backend/app/services/logs.py
@@ -9,3 +9,23 @@ class Logger:
         with self.__lock:
             print(f"ERROR: {module} - {e}")
 
+    def info(self, module: str, msg: str) -> None:
+        with self.__lock:
+            print(f"INFO: {module} - {msg}")
+    
+    def debug(self, module: str, msg: str) -> None:
+        with self.__lock:
+            print(f"DEBUG: {module} - {msg}")
+
+    def warning(self, module: str, msg: str) -> None:
+        with self.__lock:
+            print(f"WARNING: {module} - {msg}")
+        
+    def critical(self, module: str, msg: str) -> None:
+        with self.__lock:
+            print(f"CRITICAL: {module} - {msg}")
+    
+    def success(self, module: str, msg: str) -> None:
+        with self.__lock:
+            print(f"SUCCESS: {module} - {msg}")
+    
