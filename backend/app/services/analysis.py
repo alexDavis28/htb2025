1e45c589b31ec049be392e776203d9b22f144d60
miksquared <mbagi2004@gmail.com>
2025-03-02 09:16:59 +0000

    type alias

--- backend/app/services/analysis.py
+++ backend/app/services/analysis.py
@@ -18,8 +18,8 @@ def analyse_image(sat_image_path: str, lidar_image_path: str) -> None:
     """
     
     # Load images into memory
-    sat_image: cv2.typing.MatLike = cv2.imread(sat_image_path)
-    lidar_image: cv2.typing.MatLike = cv2.imread(lidar_image_path)
+    sat_image: MatLike = cv2.imread(sat_image_path)
+    lidar_image: MatLike = cv2.imread(lidar_image_path)
 
     # Check if images are loaded
     if sat_image is None:
@@ -43,7 +43,7 @@ def proportion_image_white(image: MatLike) -> float:
     count_white = np.count_nonzero(image)
     return count_white / total_pixels
  
-def percent_green(sat_image: cv2.typing.MatLike):
+def percent_green(sat_image: MatLike):
     # Define the upper and lower HSV thresholds for green
     # Array (H,S,V)
     # https://stackoverflow.com/questions/10948589/choosing-the-correct-upper-and-lower-hsv-boundaries-for-color-detection-withcv/48367205#48367205
@@ -59,24 +59,24 @@ def percent_green(sat_image: cv2.typing.MatLike):
 def average_elevation(lidar_image_path: str) -> None:
     pass
 
-def edge_density(sat_image: cv2.typing.MatLike) -> float:
+def edge_density(sat_image: MatLike) -> float:
     # Apply Canny edge detection to the satellite image
-    edges: cv2.typing.MatLike = cv2.Canny(sat_image, 110, 180)
+    edges: MatLike = cv2.Canny(sat_image, 110, 180)
     # cv2.imshow("edges", edges);cv2.waitKey();cv2.destroyAllWindows()
     return proportion_image_white(edges)
 
 
-def percent_horizontal(sat_image: cv2.typing.MatLike) -> float:
+def percent_horizontal(sat_image: MatLike) -> float:
     # Transform the image to grayscale
-    gray_image: cv2.typing.MatLike = cv2.cvtColor(sat_image, cv2.COLOR_BGR2GRAY)
+    gray_image: MatLike = cv2.cvtColor(sat_image, cv2.COLOR_BGR2GRAY)
     
     gray_image = cv2.bitwise_not(gray_image)
-    bw: cv2.typing.MatLike = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, \
+    bw: MatLike = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, \
                                 cv2.THRESH_BINARY, 15, -2)
     
     cols: int = bw.shape[1]
     horizontal_size: int = cols // 30
-    horizontal_structure: cv2.typing.MatLike = cv2.getStructuringElement(cv2.MORPH_RECT, (horizontal_size, 1))
+    horizontal_structure: MatLike = cv2.getStructuringElement(cv2.MORPH_RECT, (horizontal_size, 1))
     
     horizontal = cv2.erode(bw, horizontal_structure)
     horizontal = cv2.dilate(horizontal, horizontal_structure)
@@ -84,17 +84,17 @@ def percent_horizontal(sat_image: cv2.typing.MatLike) -> float:
     # cv2.imshow("horizontal", horizontal);cv2.waitKey();cv2.destroyAllWindows()
     return proportion_image_white(horizontal)
 
-def percent_vertical(sat_image: cv2.typing.MatLike) -> float:
+def percent_vertical(sat_image: MatLike) -> float:
     # Transform the image to grayscale
-    gray_image: cv2.typing.MatLike = cv2.cvtColor(sat_image, cv2.COLOR_BGR2GRAY)
+    gray_image: MatLike = cv2.cvtColor(sat_image, cv2.COLOR_BGR2GRAY)
     
     gray_image = cv2.bitwise_not(gray_image)
-    bw: cv2.typing.MatLike = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, \
+    bw: MatLike = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, \
                                 cv2.THRESH_BINARY, 15, -2)
     
     rows: int = bw.shape[0]
     vertical_size: int = rows // 30
-    vertical_structure = cv2.getStructuringElement(cv2.MORPH_RECT, (vertical_size, 1))
+    vertical_structure: MatLike = cv2.getStructuringElement(cv2.MORPH_RECT, (vertical_size, 1))
     
     vertical = cv2.erode(bw, vertical_structure)
     vertical = cv2.dilate(vertical, vertical_structure)
